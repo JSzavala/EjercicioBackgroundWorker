d9165882b0661ca7871efbe11865e201865b487b
jsamu <jsamu@DESKTOP-5EHEBUS>
2025-06-20 17:08:40 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioBackgroundWorker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int counter = 0;
+            int max = 41;
+
+            while (counter <= max)
+            {
+                backgroundWorker1.ReportProgress(0, counter.ToString());
+                System.Threading.Thread.Sleep(1000);
+                counter++;
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = (string)e.UserState;
+        }
+    }
+}
